b82600daf4659adc474876baf377965c2c0f47a4
SalavessaMota <nunosalavessa@hotmail.com>
2024-09-25 11:36:13 +0100

    Add colors and styles

--- SuperShop.Prism/SuperShop.Prism/Helpers/Languages.cs
+++ SuperShop.Prism/SuperShop.Prism/Helpers/Languages.cs
@@ -36,5 +36,13 @@ namespace SuperShop.Prism.Helpers
         public static string Loading => Resource.Loading;
 
         public static string SearchProduct => Resource.SearchProduct;
+
+        public static string Stock => Resource.Stock;
+
+        public static string IsAvailable => Resource.IsAvailable;
+
+        public static string LastPurchase => Resource.LastPurchase;
+
+        public static string LastSale => Resource.LastSale;
     }
 }
